706f8cf27bd8c1a4fce9afd30b2d114515c85896
unknown <velkovandrej819@gmail.com>
2022-02-17 20:29:50 +0100

    urkeekkkellekd

--- pages/premium-products/index.js
+++ pages/premium-products/index.js
@@ -56,7 +56,7 @@ export default function PremiumProducts(props) {
         </div>
         <div className="premium__brands">
           <div className="premium__brands-content center-content">
-            <Swiper
+            {/* <Swiper
               spaceBetween={10}
               slidesPerView={5}
               loop={true}
@@ -84,14 +84,14 @@ export default function PremiumProducts(props) {
                   spaceBetween: 0,
                 },
               }}
-            >
-              <SwiperSlide>
-                <img src="/Premium/Brands/ime.png" alt="" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="/Premium/Brands/biooil.png" alt="" />
-              </SwiperSlide>
-              <SwiperSlide>
+            > */}
+            {/* <SwiperSlide> */}
+            <img src="/Premium/Brands/ime.png" alt="" />
+            {/* </SwiperSlide> */}
+            {/* <SwiperSlide> */}
+            <img src="/Premium/Brands/biooil.png" alt="" />
+            {/* </SwiperSlide> */}
+            {/* <SwiperSlide>
                 <img src="/Premium/Brands/tuf.png" alt="" />
               </SwiperSlide>
               <SwiperSlide>
@@ -119,8 +119,8 @@ export default function PremiumProducts(props) {
 
               <SwiperSlide>
                 <img src="/Premium/Brands/biomelan.png" alt="" />
-              </SwiperSlide>
-            </Swiper>
+              </SwiperSlide> */}
+            {/* </Swiper> */}
           </div>
         </div>
         <div className="premium__products">
